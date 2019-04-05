e473587dbf096156dc4d13434023e1f828e825ca
Choro <oljekechoro@mail.ru>
2019-04-05 17:59:57 +0300

    feat: add IStorage

---
+++ packages/types/src/main/IStorage.ts
@@ -0,0 +1,12 @@
+export type TStorageKey = string
+export type TStorageValue = any
+export type TStorageTTL = number
+
+export interface IStorage {
+  get: (key: TStorageKey) => TStorageValue
+  set: (key: TStorageKey, value: TStorageValue, ttl?: TStorageTTL) => void
+  has: (key: TStorageKey) => boolean
+  remove: (key: TStorageKey) => void
+  size: () => number
+  reset: () => void
+}
--- packages/types/src/test/index.ts
+++ packages/types/src/test/index.ts
@@ -16,7 +16,8 @@ import {
   ICloneable,
   ICurrency,
   IPromise,
-  IPromiseConstructor
+  IPromiseConstructor,
+  IStorage
 } from '../main';
 
 // IStringMap
@@ -147,3 +148,33 @@ const nativePromiseConstructor: IPromiseConstructor = Promise;
 const bluebirdPromiseConstructor: IPromiseConstructor = BluebirdPromise;
 const nativePromise: IPromise<number> = new Promise<number>(executor);
 const bluebirdPromise: IPromise<number> = new BluebirdPromise<number>(executor);
+
+// IStorage
+
+const storage: IStorage = {
+  get: (key: string): any => key,
+  set: (key: string, value: any, ttl?: number): any => ttl || key || value,
+  has: (key: string) => {
+    key;
+    return false;
+  },
+  remove: (key: string) => {
+    key;
+  },
+  size: () => 42,
+  reset: () => {}
+};
+
+const brokenStorage: IStorage = {
+  get: (key: string): any => key,
+  set: (key: string, value: any, ttl?: number): any => ttl || key || value,
+  has: (key: string) => {  // $ExpectError
+    key;
+    return 42;
+  },
+  remove: (key: string) => {
+    key;
+  },
+  size: () => 42,
+  reset: () => {}
+};
