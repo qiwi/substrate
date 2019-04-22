1da23f2340cb6d53b0e04c43f8c70d6e7c7491e7
Anton Golub <mailbox@antongolub.ru>
2019-04-22 16:22:58 +0300

    test(facade): testgen improvements

--- packages/facade/src/gen/testgen.js
+++ packages/facade/src/gen/testgen.js
@@ -1,14 +1,14 @@
-import { sync as copySync } from 'cp-file'
+import { copySync } from 'fs-extra'
 import { sync as replaceSync } from 'replace-in-file'
 import { resolve } from 'path'
 
-const SRC = resolve(__dirname, '../../../types/src/test/index.ts')
-const DST = resolve(__dirname, '../test/index.ts')
+const SRC = resolve(__dirname, '../../../types/src/test/')
+const DST = resolve(__dirname, '../test/')
 
 copySync(SRC, DST)
 copySync(resolve(__dirname, '../../typings/index.d.ts'), resolve(__dirname, '../test/index.d.ts'))
 replaceSync({
-  files: [DST],
+  files: [`${DST}/index.ts`],
   from: ['@qiwi/substrate-types'],
   to: ['@qiwi/substrate']
 })
--- packages/types/src/test/index.ts
+++ packages/types/src/test/index.ts
@@ -1,22 +1,4 @@
 export { forEach as lodashEach, map as lodashMap, set as lodashSet, get as lodashGet } from 'lodash';
 export { each as underscoreEach, map as underscoreMap } from 'underscore';
 export { Promise as BluebirdPromise } from 'bluebird';
-
-export {
-  IStringMap,
-  IAnyMap,
-  IEventEmitter,
-  ILoggerMethod,
-  ILogger,
-  TLogLevel,
-  TUtilGet,
-  TUtilSet,
-  TUtilEach,
-  TUtilMap,
-  ICloneable,
-  ICurrency,
-  IPromise,
-  IPromiseConstructor,
-  IStorage,
-  IMoney
-} from '../main';
+export * from '@qiwi/substrate-types';
