3200dec937860f761f085654413e2cfa72e53489
Maxim Pismenskiy <m.pismenskiy@qiwi.com>
2019-05-22 03:27:36 +0300

    feat: add test for ICollection

---
+++ packages/types/src/test/ICollection.ts
@@ -0,0 +1,19 @@
+import { ICollection } from './index';
+
+const brokenCollection: ICollection<number> = {
+  isEmpty: () => 123, // $ExpectError
+  size: () => 'baz', // $ExpectError
+  remove: item => 'foo', // $ExpectError
+  add: item => ({}), // $ExpectError
+  get: (item: boolean) => item, // $ExpectError
+  clear: (item: string) => item // $ExpectError
+};
+
+const collectiom: ICollection<number> = {
+  isEmpty: () => true,
+  size: () => 123,
+  remove: item => item,
+  add: item => item,
+  get: (item: number | string) => +item,
+  clear: () => {},
+};
