b332a06c1a44808bf0bf6bd7919441a38a2f9389
Maxim Pismenskiy <m.pismenskiy@qiwi.com>
2019-06-27 14:38:10 +0300

    feat: add IMiddleware

--- packages/types/src/test/IMiddleware.ts
+++ packages/types/src/test/IMiddleware.ts
@@ -5,6 +5,6 @@ const brokenMiddleware2: IMiddleware = (res: string, req: object) => 123; // $Ex
 const brokenMiddleware3: IMiddleware = (res: boolean, req: boolean, next: boolean) => ({}); // $ExpectError
 const brokenMiddleware4: IMiddleware = 123; // $ExpectError
 
-const middleware: IMiddleware = (reg: object, res: object, next: (reg: object, res: object) => {}) => {
-  next(res, reg);
+const middleware: IMiddleware = (reg: object, res: object, next?: (reg: object, res: object) => {}) => {
+  next && next(res, reg);
 };
