df2388baab0d97c256c712ee708648f1770bf8e8
Anton Golub <mailbox@antongolub.ru>
2019-10-22 21:39:18 +0300

    chore(package): up deps

--- packages/types/src/test/ts/IConfig.ts
+++ packages/types/src/test/ts/IConfig.ts
@@ -3,11 +3,12 @@ import * as NodeConfig from 'config';
 import * as Uniconfig from '@qiwi/uniconfig-core';
 import { IConfig } from './index';
 
-const conf: IConfig = new Conf();
+// FIXME
+// const conf: IConfig = new Conf();
 
-const numberConf: IConfig<number> = new Conf<number>();
+// const numberConf: IConfig<number> = new Conf<number>();
 
-const stringConf: IConfig<string> = new Conf<string>();
+// const stringConf: IConfig<string> = new Conf<string>();
 
 const nodeConf: IConfig = NodeConfig;
 
