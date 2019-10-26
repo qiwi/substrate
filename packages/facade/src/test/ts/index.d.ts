61cfc0225017adfaa2edf80b325b42ccbf2a1554
Anton Golub <mailbox@antongolub.ru>
2019-10-26 12:57:17 +0300

    test: remove auto-generated *.d.ts

--- packages/facade/src/test/ts/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@qiwi/substrate/target/es5/facade/src/main/ts/index' {
-	export * from '@qiwi/substrate/target/es5/@qiwi/substrate-types';
-	export * from '@qiwi/substrate/target/es5/@qiwi/substrate-abstract';
-}
-declare module '@qiwi/substrate' {
-	export * from '@qiwi/substrate/target/es5/index';
-}
