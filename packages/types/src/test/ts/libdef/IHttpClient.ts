5affa20ba96be14b6adc16cdfc335c5f3b088059
Anton Golub <mailbox@antongolub.ru>
2020-02-10 00:42:37 +0300

    fix(types): let HttpReqPerform 2-3 args be optional

--- packages/types/src/test/ts/libdef/IHttpClient.ts
+++ packages/types/src/test/ts/libdef/IHttpClient.ts
@@ -9,3 +9,10 @@ export const domfetchAsFetcher: IHttpFetcher = fetch
 export const crossFetchAsFetcher: IHttpFetcher = crossFetch
 export const axiosAsFetcher: IHttpFetcher = axios
 export const axiosAsClient: IHttpClient = axios
+
+async () => {
+  interface Data {}
+  const data: Data = await axiosAsClient.get<Data>('https://example.com')
+
+  return data
+}
