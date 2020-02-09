336cb51eaa4deee891cfee9ddaaef2dc59852e58
Anton Golub <mailbox@antongolub.ru>
2020-02-09 23:27:29 +0300

    chore(package): up deps

--- packages/types/src/test/ts/libdef/IPromise.ts
+++ packages/types/src/test/ts/libdef/IPromise.ts
@@ -1,4 +1,4 @@
-import * as BluebirdPromise from 'bluebird'
+import BluebirdPromise from 'bluebird'
 import { IPromise, IPromiseConstructor } from '.'
 
 const executor = (resolve: (value: number) => void, reject: (value: number) => void) => {
