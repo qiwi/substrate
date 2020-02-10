79c5ae5e4f007f1dfb8587b4336322c276c19db1
Anton Golub <mailbox@antongolub.ru>
2020-02-10 23:39:54 +0300

    doc: update licenses

--- packages/types/src/test/ts/libdef/IHttpClient.ts
+++ packages/types/src/test/ts/libdef/IHttpClient.ts
@@ -12,5 +12,7 @@ export const axiosAsClient: IHttpClient = axios
 async () => {
   type Data = { foo: string }
   const data1 = (await axiosAsClient.get<Data>('https://example.com')).data
-  return { foo: 1 }
+  const data2: typeof data1 = {foo: 1} // $ExpectError
+
+  return !!data2
 }
