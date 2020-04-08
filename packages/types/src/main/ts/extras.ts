4ccfe18eff9189811702c524f7c5c81ff238f441
Anton Golub <mailbox@antongolub.ru>
2020-04-08 11:29:13 +0300

    feat(types): add IStorageExtra as IStoreExtra alias

--- packages/types/src/main/ts/extras.ts
+++ packages/types/src/main/ts/extras.ts
@@ -2,3 +2,4 @@ import { IExtra } from './IExtra'
 import { IStorage } from './IStorage'
 
 export type IStoreExtra = IExtra<'store', IStorage>
+export type IStorageExtra = IExtra<'storage', IStorage>
--- packages/types/src/test/ts/libdef/extras.ts
+++ packages/types/src/test/ts/libdef/extras.ts
@@ -1,19 +1,20 @@
 import {
   IStoreExtra,
+  IStorageExtra,
 } from '.'
 
-const withStorage: IStoreExtra = {
-  store: {
-    get: (key: string): any => key,
-    set: (key: string, value: any, ttl?: number): any => ttl || key || value,
-    has: (key: string) => {
-      key
-      return false
-    },
-    remove: (key: string) => {
-      key
-    },
-    size: () => 42,
-    reset: () => {},
+const storage = {
+  get: (key: string): any => key,
+  set: (key: string, value: any, ttl?: number): any => ttl || key || value,
+  has: (key: string) => {
+    key
+    return false
   },
+  remove: (key: string) => {
+    key
+  },
+  size: () => 42,
+  reset: () => {},
 }
+const withStore: IStoreExtra = { store: storage }
+const withStorage: IStorageExtra = { storage }
