6367a1e1776a21f572a1f8e7099d835839460f03
Anton Golub <mailbox@antongolub.ru>
2021-04-21 16:57:09 +0300

    feat(types): add INullable generic
    closes #422

--- packages/types/src/main/ts/INil.ts
+++ packages/types/src/main/ts/INil.ts
@@ -1,3 +1,8 @@
 export type INil = null | undefined
 
 export type INullOrUndefined = INil
+
+export type INullable<T = any> = T | null
+
+export type INilable<T = any> = T | INil
+
--- packages/types/src/test/ts/libdef/INil.ts
+++ packages/types/src/test/ts/libdef/INil.ts
@@ -1,6 +1,16 @@
-import { INil, INullOrUndefined } from '.'
+import { INil, INullOrUndefined, INullable, INilable } from '.'
 
 export const n0: INil = null
-export const n1: INil = undefined
-export const n2: INil = 0 // $ExpectError
-export const n3: INil = '' // $ExpectError
+export const n1: INullOrUndefined = undefined
+export const n2: INil = 0   // $ExpectError
+export const n3: INil = ''  // $ExpectError
+
+export const n5: INullable<string> = null
+export const n6: INullable<string> = 'foo'
+export const n7: INullable<string> = 1 // $ExpectError
+export const n8: INullable<string> = undefined // $ExpectError
+
+export const n9: INilable<number> = undefined
+export const n10: INilable<number> = null
+export const n11: INilable<number> = 1
+export const n12: INilable<number> = 'foo' // $ExpectError
